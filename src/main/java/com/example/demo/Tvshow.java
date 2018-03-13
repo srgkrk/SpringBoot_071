7ca27985f93a3814ee63303b8e8ed775cc05aa69
srgkrk <srgkrk@gmail.com>
2018-03-13 10:16:23 -0400

    changed

--- src/main/java/com/example/demo/Tvshow.java
+++ src/main/java/com/example/demo/Tvshow.java
@@ -22,5 +22,36 @@ public class Tvshow {
     private  String description;
 
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
 
